e47bf6d7805447f3b8c9761f4013ec1923120e55
Your Name <you@example.com>
2025-02-12 04:22:20 -0800

    [add] 测试yaml-cpp库的使用

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # orange
 从零开始实现salar框架
+
+``` shell
+sudo apt install libboost1.67-dev libyaml-cpp-dev -y
+```
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,11 +1,38 @@
 #include "src/config.h"
 #include "src/log.h"
+#include <yaml-cpp/yaml.h>
 
 using namespace orange;
 
 ConfigVar<int>::ptr g_int_value_config = Config::Lookup("system.port", int(800), "system port");
 ConfigVar<float>::ptr g_float_value_config = Config::Lookup("system.float", 1.1111f, "system float");
 ConfigVar<char>::ptr g_char_value_config = Config::Lookup("system.char", 'a', "system char");
+
+// yaml-cpp 的简单使用
+void print_yaml(const YAML::Node& node, int level){
+   if(node.IsScalar()) {
+        ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << std::string(level*4, ' ') << node.Scalar();
+   } else if(node.IsNull()) {
+        ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << std::string(level*4, ' ') << "NULL";
+   } else if(node.IsMap()) {
+        for(auto it = node.begin(); it != node.end(); ++it){
+            ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << std::string(level*4, ' ') << "map-key:" << it->first;
+            print_yaml(it->second, level + 1);
+        }
+   } else if(node.IsSequence()){
+        ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << std::string(level*4, ' ') << "sequence:";
+        for(size_t i = 0; i < node.size(); ++i){
+            ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << std::string(level*4, ' ') << "sequence[" << i << "]:";
+            print_yaml(node[i], level + 1);
+        }
+   }
+}
+
+void tes_yaml(){
+    YAML::Node root = YAML::LoadFile("./config/log.yaml");
+    ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << "\n" << root;
+    print_yaml(root, 0);
+}
 
 int main(int argc, char** argv){
 
@@ -17,6 +44,8 @@ int main(int argc, char** argv){
 
     ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << g_char_value_config->getValue();
     ORANGE_LOG_INFO(ORANGE_LOG_ROOT()) << g_char_value_config->toString();
+
+    tes_yaml();
 
     return 0;
 }
